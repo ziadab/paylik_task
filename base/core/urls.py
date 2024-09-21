f2e680504b4feda33ac75587e6179dafb01f3a27
Ziad Abouelfarah <zain.fake02@gmail.com>
2024-09-21 04:14:28 +0100

    feat: handle refresh token

--- base/core/urls.py
+++ base/core/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 from .views import RegisterView, LoginView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
+    path("refresh/", TokenRefreshView.as_view(), name="refresh"),
 ]
