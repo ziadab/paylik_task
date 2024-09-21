0d4cce805c5c6c63c10c2b6b7592553f2f3dec5d
Ziad Abouelfarah <zain.fake02@gmail.com>
2024-09-21 01:40:40 +0100

    feat: finish auth

---
+++ base/core/views.py
@@ -0,0 +1,62 @@
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework.authentication import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.models import User
+from .serializers import UserSerializer, LoginSerializer
+
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            try:
+                user = User.objects.get(email=serializer.validated_data["email"])
+            except User.DoesNotExist:
+                return Response(
+                    {"message": "No user with this email found."},
+                    status=status.HTTP_401_UNAUTHORIZED,
+                )
+            user = authenticate(
+                username=user.username,
+                password=serializer.validated_data["password"],
+            )
+            if user:
+                refresh = RefreshToken.for_user(user)
+                return Response(
+                    {
+                        "message": "Login successful",
+                        "access_token": str(refresh.access_token),
+                        "refresh": str(refresh),
+                    },
+                    status=status.HTTP_200_OK,
+                )
+            else:
+                return Response(
+                    {"message": "Invalid credentials"},
+                    status=status.HTTP_401_UNAUTHORIZED,
+                )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            return Response(
+                {
+                    "message": "User created successfully",
+                    "access_token": str(refresh.access_token),
+                    "refresh": str(refresh),
+                },
+                status=status.HTTP_201_CREATED,
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
